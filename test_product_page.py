422c2cdc6758f972495257b89c505b0ba09ab921
hypno-spira <64416493+hypno-spira@users.noreply.github.com>
2021-04-08 22:49:51 +0700

    find the link with the bug and mark it as xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,28 @@
 from .pages.product_page import ProductPage
 import time
+import pytest
+
+linktail = [
+    "?promo=offer0",
+    "?promo=offer1",
+    "?promo=offer2",
+    "?promo=offer3",
+    "?promo=offer4",
+    "?promo=offer5",
+    "?promo=offer6",
+    pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+    "?promo=offer8",
+    "?promo=offer9"
+]
 
 
-def test_adding_to_basket_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link_tail', linktail)
+def test_guest_can_add_product_to_basket(browser, link_tail):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{link_tail}"
     page = ProductPage(browser, link)
     page.open()  # открываем страницу
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-    time.sleep(5)
+    #time.sleep(5)
     page.match_names_of_books()
     page.match_prices_of_books()
