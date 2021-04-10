9d43ff3819c330b2e1b8d5e7dcb3207790fa7ee7
hypno-spira <64416493+hypno-spira@users.noreply.github.com>
2021-04-10 14:36:26 +0700

    added "basket is empty" methods

---
+++ pages/basket_page.py
@@ -0,0 +1,12 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def should_be_text_about_basket_is_empty(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_IS_EMPTY_TEXT), \
+            "Должно быть сообщение о том, что корзина пуста"
+
+    def should_be_no_items_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), \
+            "В корзине должно быть пусто!"
--- pages/locators.py
+++ pages/locators.py
@@ -22,3 +22,9 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group > a")
+
+
+class BasketPageLocators():
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
+    BASKET_IS_EMPTY_TEXT = (By.CSS_SELECTOR, "#content_inner")
